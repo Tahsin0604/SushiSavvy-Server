c4068061db848dba4a4dc30c6612dc52f4474352
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-02 13:35:02 +0600

    get chefs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const chef = require("./data/chef.json");
 app.get("/", (req, res) => {
   res.send("Chef Recipe Hunter Api");
+});
+app.get("/chefs", (req, res) => {
+  res.send(chef);
 });
 app.listen(port, () => {
   console.log(`Port number: ${port}`);
