ced51c4086e8cc7cd6ac9a433bb7674e66881b5c
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-02 13:47:21 +0600

    get single chef

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get("/", (req, res) => {
 app.get("/chefs", (req, res) => {
   res.send(chef);
 });
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = chef.find((c) => parseInt(c.id) === parseInt(id));
+  console.log(selectedChef);
+  res.send(selectedChef);
+});
 app.listen(port, () => {
   console.log(`Port number: ${port}`);
 });
