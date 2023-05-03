6cdd375eee56bf59153b9d341c781a017115870f
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-03 17:27:00 +0600

    api update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const chef = require("./data/chef.json");
+const recipes = require("./data/recipe.json");
 //node cors
 var cors = require("cors");
 app.use(cors());
@@ -10,13 +11,19 @@ app.use(cors());
 app.get("/chefs", (req, res) => {
   res.send(chef);
 });
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
 
 //get details chefs data
 app.get("/chefs/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id);
+
   const selectedChef = chef.find((c) => parseInt(c.id) === parseInt(id));
-  console.log(selectedChef);
+  const signatureDishes = recipes.filter(
+    (r) => parseInt(r.chef_id) === parseInt(id)
+  );
+  selectedChef["signatureDishes"] = signatureDishes;
   res.send(selectedChef);
 });
 
